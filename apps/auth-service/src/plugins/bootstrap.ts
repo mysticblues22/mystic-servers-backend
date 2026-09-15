7df1768bb8d20544dea423362c41868c081d72f7
Vishnu R <rdhanush07@gmail.com>
2026-09-15 22:01:00 +0530

    fix(auth-service): update bootstrap to load local .env file by default

--- apps/auth-service/src/plugins/bootstrap.ts
+++ apps/auth-service/src/plugins/bootstrap.ts
@@ -1,7 +1,5 @@
 import { bootstrap } from "@mystic/bootstrap";
 
 export function initializeBootstrap(): void {
-  bootstrap({
-    envFile: "/srv/git/infrastructure/env/backend.env",
-  });
+  bootstrap();
 }
